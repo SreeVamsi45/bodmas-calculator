3a5dffb116b8cc2796de4f057ecee604c59596dc
vamsi <vsvam45@gmail.com>
2024-12-09 14:12:24 +0530

    intial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Vaddadi Sree Vamsi-BODMAS Calculator-0.1";
+const contentToCache = [
+    "Build/calculator webglbuild.loader.js",
+    "Build/calculator webglbuild.framework.js.gz",
+    "Build/calculator webglbuild.data.gz",
+    "Build/calculator webglbuild.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
